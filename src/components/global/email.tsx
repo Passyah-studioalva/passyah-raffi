9b8f28f7fee4884b717b7b190f404f3318824c98
Passyah Raffi <passyah@studioalva.co>
2023-09-19 14:10:07 +0700

    update form email

--- src/components/global/email.tsx
+++ src/components/global/email.tsx
@@ -9,13 +9,51 @@ import {
   GridItem,
   Textarea,
   Text,
+  Box,
+  FormHelperText,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { Form, Field, useField, useForm } from "react-final-form";
 
 const Email: React.FC = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const lineBreak = "%0D%0A";
+  const urlMail = "passyah11@gmail.com";
+
+  const [name, setName] = useState(``);
+  const [email, setEmail] = useState(``);
+  const [message, setMessage] = useState(``);
+  const [errorName, setErrorName] = useState(false);
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  const sendEmail = `mailto:${urlMail}?subject=Hello, My name ${name} ${email}&body=${message.replaceAll(
+    "\n",
+    lineBreak
+  )}`;
+
+  const handleSend = () => {
+    if (!name) {
+      setErrorMessage(true);
+    }
+
+    if (!email) {
+      setErrorName(true);
+    }
+
+    if (!message) {
+      setErrorEmail(true);
+    }
+
+    if (name && email && message) {
+      setErrorName(false);
+      setErrorEmail(false);
+      setErrorMessage(false);
+      window.open(sendEmail);
+    }
+
+    // window.open(sendEmail);
+  };
 
   return (
     <Container
@@ -28,83 +66,64 @@ const Email: React.FC = () => {
       <Text as={"h2"} fontSize={50} px={1} mb={10}>
         Send me an email
       </Text>
-      <Grid
-        templateColumns={[
-          "repeat(1, 1fr)",
-          "repeat(1, 1fr)",
-          "repeat(1, 1fr)",
-          "repeat(2, 1fr)",
-        ]}
-        templateRows={[
-          "repeat(1, 1fr)",
-          "repeat(1, 1fr)",
-          "repeat(1, 1fr)",
-          "repeat(2, 1fr)",
-        ]}
-        gap={[0, 0, 0, 5]}
-        pb={1}
-      >
-        <GridItem>
-          <FormControl isRequired p={1}>
-            <FormLabel as={"p"}>Name</FormLabel>
-            <Input
-              type="text"
-              value={name}
-              autoComplete={"off"}
-              placeholder="input name"
-              fontSize={20}
-              py={7}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </FormControl>
+
+      <FormControl isRequired p={1} pb={1} className="container">
+        <GridItem className="name">
+          <FormLabel as={"p"}>Name</FormLabel>
+          <Input
+            type="text"
+            autoComplete={"off"}
+            placeholder="input name"
+            fontSize={20}
+            py={7}
+            value={name}
+            border={"1px"}
+            borderColor={errorName ? "red.500" : "gray.200"}
+            onChange={(e) => setName(e.target.value)}
+          />
+          {errorName && (
+            <FormHelperText color={"red.500"}>Name is required.</FormHelperText>
+          )}
         </GridItem>
-        <GridItem
-          display={["none", "none", "none", "flex"]}
-          rowSpan={[0, 0, 0, 2]}
-        >
-          <FormControl isRequired p={1}>
-            <FormLabel as={"p"}>Message</FormLabel>
-            <Textarea
-              placeholder="input message"
-              resize={"none"}
-              height={"190px"}
-              fontSize={20}
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-          </FormControl>
+
+        <GridItem className="email">
+          <FormLabel as={"p"}>Email</FormLabel>
+          <Input
+            type="email"
+            autoComplete={"off"}
+            placeholder="input email"
+            fontSize={20}
+            py={7}
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {errorEmail && (
+            <FormHelperText color={"red.500"}>
+              Email is required.
+            </FormHelperText>
+          )}
         </GridItem>
-        <GridItem>
-          <FormControl isRequired p={1}>
-            <FormLabel as={"p"}>Email</FormLabel>
-            <Input
-              type="email"
-              value={email}
-              autoComplete={"off"}
-              placeholder="input email"
-              fontSize={20}
-              py={7}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </FormControl>
+
+        <GridItem className="message">
+          <FormLabel as={"p"}>Message</FormLabel>
+          <Textarea
+            placeholder="input message"
+            autoComplete={"off"}
+            resize={"none"}
+            height={"190px"}
+            fontSize={20}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          {errorMessage && (
+            <FormHelperText color={"red.500"}>
+              Message is required.
+            </FormHelperText>
+          )}
         </GridItem>
-        <GridItem
-          display={["flex", "flex", "flex", "none"]}
-          rowSpan={[0, 0, 0, 2]}
-        >
-          <FormControl isRequired p={1}>
-            <FormLabel as={"p"}>Message</FormLabel>
-            <Textarea
-              placeholder="input message"
-              resize={"none"}
-              height={"190px"}
-              fontSize={20}
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-          </FormControl>
-        </GridItem>
-      </Grid>
+      </FormControl>
+
       <Flex justifyContent={"end"} p={1} mt={5}>
         <Button
           colorScheme="teal"
@@ -112,6 +131,7 @@ const Email: React.FC = () => {
           rounded={"md"}
           fontSize={25}
           p={6}
+          onClick={() => handleSend()}
         >
           Send email
         </Button>
