b4c541cac95b716347e8b1324c261a9e2af81933
Passyah Raffi <passyah@studioalva.co>
2023-11-09 12:29:15 +0700

    add new tag meta google ads

--- src/components/layout/meta-head/index.tsx
+++ src/components/layout/meta-head/index.tsx
@@ -11,6 +11,8 @@ export default function MetaHead() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        {/* google ads */}
+        <meta name="google-adsense-account" content="ca-pub-1523860768772432"></meta>
       </Head>
     </>
   );
