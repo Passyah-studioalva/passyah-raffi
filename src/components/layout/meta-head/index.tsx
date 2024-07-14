bbcea3c186e9ff19d37434b91acc28a26fbe7218
PassyahRaffi <passyah@studioalva.co>
2024-07-15 00:09:03 +0700

    update

--- src/components/layout/meta-head/index.tsx
+++ src/components/layout/meta-head/index.tsx
@@ -10,7 +10,7 @@ export default function MetaHead() {
           content="I'm a front-end developer based in DKI Jakarta, Jakarta Selatan, Indonesia. Since 2022 I've known programs to explore HTML, CSS, and Javascript. I focused on studying at the Dumbways.id bootcamp and until now I work as a frontend developer team at studioalva.co."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="./3dPR.svg" />
         {/* google ads */}
         <meta name="google-adsense-account" content="ca-pub-1523860768772432"></meta>
       </Head>
